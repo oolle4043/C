21d075e6ad23bca99f5b9c1bd7235cae1884413b
Ancha <oolle4043@gmail.com>
2021-08-27 22:30:34 +0900

    upload

---
+++ Chap12/Programming/05.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <ctype.h>
+#include <stdio.h>
+
+int main()
+{
+	int c;
+
+	printf("문자를 입력하시오: ");
+
+	while ((c = getchar()) != '.')
+	{
+		if (islower(c)) {
+			c = toupper(c);
+			putchar(c);
+			printf("\n문자를 입력하시오: ");
+		}
+		else if(isupper(c)){
+			c = tolower(c);
+			putchar(c);
+			printf("\n문자를 입력하시오: ");
+		}
+		
+	}
+
+	return 0;
+}
---
+++ Chap12/Programming/06.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <ctype.h>
+#include <stdio.h>
+#define SIZE 40
+
+void str_upper(char* s);
+
+int main()
+{
+	char s[SIZE] = "";
+
+	printf("문자열을 입력하시오: ");
+	gets(s);
+
+	printf("변환된 문자열: ");
+	str_upper(s);
+
+	return 0;
+}
+
+void str_upper(char* s)
+{
+	for (int i = 0; s[i] != NULL; i++) {
+		s[i] = toupper(s[i]);
+		printf("%c", s[i]);
+	}
+}
