a6cfa15785824942cd4c145621dfa759fa239f2a
Ancha <oolle4043@gmail.com>
2021-08-27 23:50:44 +0900

    upload

---
+++ Chap12/Programming/07.c
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#define SIZE 5
+
+int get_respones(char* prompt);
+
+int main()
+{
+	char prompt[SIZE] = "";
+
+	printf("게임을 하시겠습니까?: ");
+	gets(prompt);
+
+	if (get_respones(prompt) == 1)
+		printf("긍정적인 대답");
+	else if (get_respones(prompt) == 0)
+		printf("부정적인 대답");
+
+	return 0;
+}
+
+int get_respones(char* prompt)
+{
+	int n;
+	char yes[] = "yes";
+	char ok[] = "ok";
+	char no[] = "no";
+
+	for (int i = 0; prompt[i] != NULL; i++)
+		prompt[i] = tolower(prompt[i]);
+
+	if (strcmp(prompt, yes) == 0)
+		return 1;
+	else if (strcmp(prompt, ok) == 0)
+		return 1;
+	else
+		return 0;
+
+}
---
+++ Chap12/Programming/08.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#define SIZE 100
+
+int main()
+{
+	char s[SIZE] = "";
+	char space[] = " ";
+
+	printf("문자열을 입력하시오: ");
+	gets(s);
+
+	char* token = strtok(s, space);
+	int count = 0;
+
+	while (token != NULL){
+		count++;
+		token = strtok(NULL, space);
+	}
+
+	printf("단어의 수는 %d입니다.", count);
+
+	return 0;
+}
