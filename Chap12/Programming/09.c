6ad8f3baeae7cf53fb08bd39e2937b1f07f16b98
Ancha <oolle4043@gmail.com>
2021-08-28 00:35:48 +0900

    upload

---
+++ Chap12/Programming/09.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#define SIZE 100
+
+int main()
+{
+	char s[SIZE] = "";
+
+	printf("텍스트를 입력하세요: ");
+	gets(s);
+
+	if (islower(s[0]))
+		s[0] = toupper(s[0]);
+
+	s[strlen(s)] += '.';
+
+	printf("수정된 텍스트: %s", s);
+
+	return 0;
+}
---
+++ Chap12/Programming/10.c
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#define SIZE 100
+
+int main()
+{
+	char s[SIZE] = "";
+	int n = 0;
+
+	printf("문자열을 입력하시오: ");
+	gets(s);
+
+	for (int i = 0; s[i] != NULL; i++) {
+		if (isupper(s[i]))
+			s[i] = tolower(s[i]);
+	}
+	for (int i = 0; i < strlen(s) / 2; i++) {
+		if (s[i] == s[strlen(s) - i - 1])
+			continue;
+		else
+			n++;
+	}
+
+	if (n == 0)
+		printf("회문입니다.");
+	else
+		printf("회문이 아닙니다.");
+
+	return 0;
+}
