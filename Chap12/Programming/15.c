84d36246f31b641b997553c3e09d0edb7c1b95a1
Ancha <oolle4043@gmail.com>
2021-08-31 23:23:21 +0900

    upload

---
+++ Chap12/Programming/15.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#define SIZE 10
+
+int main()
+{
+	char calculate[SIZE];
+	int x, y, result;
+
+	printf("연산을입력하시오: ");
+	scanf("%s %d %d",&calculate, &x, &y);
+
+	if (!strcmp(calculate, "add"))
+		printf("연산결과: %d", x + y);
+	else if (!strcmp(calculate, "sub"))
+		printf("연산결과: %d", x - y);
+	else if (!strcmp(calculate, "mul"))
+		printf("연산결과: %d", x * y);
+	else if (!strcmp(calculate, "div"))
+		printf("연산결과: %lf", (double)x / y);
+
+	return 0;
+}
---
+++ Chap12/Programming/16.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <string.h>
+#define SIZE 100
+
+int main() {
+	char s[SIZE];
+	int size;
+
+	puts("광고하고 싶은 텍스트를 입력하시오: ");
+	gets(s);
+
+	size = strlen(s);
+
+	puts("======================");
+
+	for (int i = 0; i < size; i++) {
+		for (int j = i; j < (i + size); j++) {
+			if (j < size)
+				putchar(s[j]); else {
+				putchar(s[j - size]);
+			}
+		}
+		putchar('\n');
+	}
+
+	return 0;
+
+}
