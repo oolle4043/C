f43a81f18fc9c230100db29af27a59ee8a587b12
Ancha <80835866+oolle4043@users.noreply.github.com>
2021-09-23 15:27:58 +0900

    Update 02.c

--- C/Chap09/Programming/02.c
+++ C/Chap09/Programming/02.c
@@ -1,4 +1,4 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -7,8 +7,6 @@ void get_dice_face();
 
 int main()
 {
-	srand((unsigned)time(NULL));
-
 	get_dice_face();
 
 	return 0;
@@ -16,36 +14,33 @@ int main()
 
 void get_dice_face()
 {
-	static int d_1;
-	static int d_2;
-	static int d_3;
-	static int d_4;
-	static int d_5;
-	static int d_6;
+	srand((unsigned)time(NULL));
+	static int d1, d2, d3, d4, d5, d6;
+
 	int c;
-	
+
 	for (int i = 0; i < 100; i++) {
 		c = rand() % 6 + 1;
 		switch (c) {
 		case 1:
-			d_1++;
+			d1++;
 			break;
 		case 2:
-			d_2++;
+			d2++;
 			break;
 		case 3:
-			d_3++;
+			d3++;
 			break;
 		case 4:
-			d_4++;
+			d4++;
 			break;
 		case 5:
-			d_5++;
+			d5++;
 			break;
 		case 6:
-			d_6++;
+			d6++;
 			break;
 		}
 	}
-	printf("1 -> %d\n2 -> %d\n3 -> %d\n4 -> %d\n5 -> %d\n6 -> %d\n", d_1, d_2, d_3, d_4, d_5, d_6);
+	printf("1 -> %d\n2 -> %d\n3 -> %d\n4 -> %d\n5 -> %d\n6 -> %d\n", d1, d2, d3, d4, d5, d6);
 }
