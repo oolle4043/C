cb09f7080b836743c9abd6652e2b5559d0901551
Ancha <80835866+oolle4043@users.noreply.github.com>
2021-12-04 00:02:27 +0900

    upload

---
+++ C/Chap13/Programming/11.c
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+typedef struct list {
+	char name[10];
+	char home_num[20];
+	char phone_num[20];
+}LIST;
+
+int main()
+{
+	LIST list_array[3];
+	char search_name[10];
+
+	for (int i = 0; i < 3; i++) {
+		printf("이름을 입력하시오: ");
+		scanf("%s", list_array[i].name);
+		printf("집전화번호를 입력하시오: ");
+		scanf("%s", list_array[i].home_num);
+		printf("휴대폰번호를 입력하시오: ");
+		scanf("%s", list_array[i].phone_num);
+	}
+
+	printf("검색할 이름을 입력하시오: ");
+	scanf("%s", search_name);
+
+	for (int i = 0; i < 3; i++) {
+		if (strcmp(list_array[i].name, search_name) == 0)
+			printf("집전화번호: %s\n휴대폰번호: %s", list_array[i].home_num, list_array[i].phone_num);
+	}
+	  
+	return 0;
+}
