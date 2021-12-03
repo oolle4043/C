acf44061ef62f2141597193b0f681586d13d30e6
Ancha <80835866+oolle4043@users.noreply.github.com>
2021-12-04 00:00:47 +0900

    upload

---
+++ C/Chap13/Programming/09.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+typedef struct food {
+	char name[100];
+	int calories;
+}FOOD;
+
+int main()
+{
+	FOOD food_array[3] = { {"밥",700},
+							{"커피", 700},
+							{"간식", 700} };
+
+	printf("총 칼로리 = %d", food_array[0].calories + food_array[1].calories + food_array[2].calories);
+
+	return 0;
+}
