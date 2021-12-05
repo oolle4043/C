de452e8b08f7b62cbaebaafc2d8081bf9178db47
Ancha <80835866+oolle4043@users.noreply.github.com>
2021-12-05 20:40:42 +0900

    Use malloc

---
+++ C/Chap13/Programming/etc/struct_Student.c
@@ -0,0 +1,39 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct student {
+	int number;
+	char name[10];
+	double grade;
+}S;
+
+int main()
+{
+	int size;
+
+	printf("몇명을 입력하시겠습니까? ");
+	scanf("%d", &size);
+	printf("\n");
+	
+	S* list = malloc(sizeof(S) * size);
+
+	for (int i = 0; i < size; i++) {
+		printf("번호를 입력하세요: ");
+		scanf("%d", &list[i].number);
+		printf("이름을 입력하세요: ");
+		scanf("%s", &list[i].name);
+		printf("성적을 입력하세요: ");
+		scanf("%lf", &list[i].grade);
+		printf("============================\n");
+	}
+
+	for (int i = 0; i < size; i++) {
+		printf("번호: %d, 이름: %s, 성적: %lf\n", list[i].number, list[i].name, list[i].grade);
+	 }
+
+	free(list);
+
+	return 0;
+}
