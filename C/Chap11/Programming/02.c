05afdbe0071cd15e82b1bf5fa53986854a85cfc8
Ancha <80835866+oolle4043@users.noreply.github.com>
2021-12-07 18:20:15 +0900

    Update 02.c

--- C/Chap11/Programming/02.c
+++ C/Chap11/Programming/02.c
@@ -5,12 +5,12 @@ void get_sum_diff(int x, int y, int* p_sum, int* p_diff);
 int main()
 {
 	int x = 100, y = 200;
-	int* p_sum = 0;
-	int* p_diff = 0;
+	int sum = 0;
+	int diff = 0;
 
-	get_sum_diff(x, y, &p_sum, &p_diff);
+	get_sum_diff(x, y, &sum, &diff);
 
-	printf("원소들의 합 = %d\n원소들의 차 = %d\n", p_sum, p_diff);
+	printf("원소들의 합 = %d\n원소들의 차 = %d\n", sum, diff);
 
 	return 0;
 }
