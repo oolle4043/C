652522adca8c068911f46dcc7644f66213802882
Ancha <80835866+oolle4043@users.noreply.github.com>
2022-05-02 21:43:28 +0900

    Ex04_01

---
+++ Java/Song.java
@@ -0,0 +1,17 @@
+public class Song {
+	String title;
+	
+	public Song(String title){
+		this.title = title;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public static void main(String[] args) {
+		Song mySong = new Song("Nessun Dorma");
+		Song yourSong = new Song("공주는 잠 못 이루고");
+		System.out.println("내 노래는 " + mySong.title);
+		System.out.println("너 노래는 " + yourSong.title);
+	}
+}
