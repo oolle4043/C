b04f388983702d86b884558c21225792c77b2177
Ancha <80835866+oolle4043@users.noreply.github.com>
2022-05-23 14:36:30 +0900

    Relay
    Relay

---
+++ Arduino/Capstone_02/Relay.ino
@@ -0,0 +1,30 @@
+int Relaypin1 = 7;
+int Relaypin2 = 6;
+int Relaypin3 = 5;
+int Relaypin4 = 4;
+
+void setup() {
+  pinMode(Relaypin1, OUTPUT);
+//  pinMode(Relaypin2, OUTPUT);
+//  pinMode(Relaypin3, OUTPUT);
+//  pinMode(Relaypin4, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(Relaypin1, LOW);
+  delay(1000);
+//  digitalWrite(Relaypin1, HIGH);
+//  delay(1000);
+//  digitalWrite(Relaypin2, LOW);
+//  delay(1000);
+//  digitalWrite(Relaypin2, HIGH);
+//  delay(1000);
+//  digitalWrite(Relaypin3, LOW);
+//  delay(1000);
+//  digitalWrite(Relaypin3, HIGH);
+//  delay(1000);
+//  digitalWrite(Relaypin4, LOW);
+//  delay(1000);
+//  digitalWrite(Relaypin4, HIGH);
+//  delay(1000);
+}
