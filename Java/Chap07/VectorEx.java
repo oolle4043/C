1c59f58b5629a248c2878f9fb5d03d371bc9a3d9
Ancha <80835866+oolle4043@users.noreply.github.com>
2022-06-03 23:04:40 +0900

    01

---
+++ Java/Chap07/VectorEx.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class VectorEx {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Vector<Double> v = new Vector<Double>();
+
+		for (int i = 0; i < 5; i++) {
+			v.add(Double.valueOf(sc.next()));
+		}
+		double max = v.get(0);
+		
+		for(int i = 1; i < v.size(); i++) {
+			if(max < v.get(i)) {
+				max = v.get(i);
+			}
+		}
+		System.out.println("가장 큰 수는 " + max);
+		
+		sc.close();
+	}
+
+}
