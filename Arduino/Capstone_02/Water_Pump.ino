a967f8f4be94e16e985d584efd9417a897087d15
Ancha <80835866+oolle4043@users.noreply.github.com>
2022-06-13 20:59:43 +0900

    Water_Pump

---
+++ Arduino/Capstone_02/Water_Pump.ino
@@ -0,0 +1,15 @@
+#define ENA 5
+#define IN1 6
+#define IN2 7
+
+void setup() {
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ENA, HIGH);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+}
